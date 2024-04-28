916a52af449204f2acc68a28819976f4e5f48e9f
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-04-28 09:57:21 -0300

    Finish MLX model

--- mlx-models/BasicLM.py
+++ mlx-models/BasicLM.py
@@ -5,6 +5,8 @@ import mlx.nn as nn
 import mlx.core as mx
 import mlx.optimizers as optim
 import numpy as np
+from functools import partial
+import math
 
 
 class TransformerLM(nn.Module):
@@ -60,7 +62,8 @@ def iterate_batches(batch_size, context_size, dataset):
             s = 0
 
 
-def train(num_blocks, dim, num_heads, checkpoint, learning_rate, weight_decay):
+def train(num_blocks, batch_size,
+          context_size, dim, num_heads, checkpoint, learning_rate, weight_decay, num_iters, lr_warmup):
     vocab, train, valid, test = load_ptb()
 
     model = TransformerLM(
@@ -76,3 +79,48 @@ def train(num_blocks, dim, num_heads, checkpoint, learning_rate, weight_decay):
     optimizer = optim.AdamW(
         learning_rate=learning_rate, weight_decay=weight_decay
     )
+
+    def eval_fn(dataset):
+        inputs, targets = map(mx.array, to_samples(context_size, dataset))
+        loss = 0
+        for s in range(0, targets.shape[0], batch_size):
+            bx, by = inputs[s: s + batch_size], targets[s: s + batch_size]
+            bx, by = map(mx.array, (bx, by))
+            losses = loss_fn(model, bx, by, reduce=False)
+            loss += mx.sum(losses).item()
+        return loss / len(targets)
+
+    state = [model.state, optimizer.state]
+
+    @partial(mx.compile, inputs=state, outputs=state)
+    def step(inputs, targets):
+        loss_and_grad_fn = nn.value_and_grad(model, loss_fn)
+        loss, grads = loss_and_grad_fn(model, inputs, targets)
+        optimizer.update(model, grads)
+        return loss
+
+    train_iterator = iterate_batches(batch_size, context_size, train)
+    losses = []
+    for it, (inputs, targets) in zip(range(num_iters), train_iterator):
+        inputs, targets = map(mx.array, (inputs, targets))
+        optimizer.learning_rate = min(1, it / lr_warmup) * learning_rate
+        loss = step(inputs, targets)
+        mx.eval(state)
+        losses.append(loss.item())
+        # TODO: Remove prints when everything is working.
+        # concatenate everything to an array of strings and print at the end.
+        if (it + 1) % 5 == 0:
+            train_loss = np.mean(losses)
+            print(
+                f"Iter {it + 1}: Train loss {train_loss:.3f}, "
+            )
+            val_loss = eval_fn(valid)
+            print(
+                f"Iter {it + 1}: "
+                f"Val loss {val_loss:.3f}, "
+                f"Val ppl {math.exp(val_loss):.3f}, "
+            )
+
+    test_loss = eval_fn(test)
+    test_ppl = math.exp(test_loss)
+    print(f"Test loss {test_loss:.3f}, Test ppl {test_ppl:.3f}.")

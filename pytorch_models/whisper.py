604423fb24fc4ed19fe63a5bdffb044b41efd762
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-05-12 17:06:04 -0300

    Start whisper model

---
+++ pytorch_models/whisper.py
@@ -0,0 +1,22 @@
+from datasets import load_dataset
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+import torch
+
+device = torch.device('mps')
+ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+audio_sample = ds[0]["audio"]
+waveform = audio_sample["array"]
+sampling_rate = audio_sample["sampling_rate"]
+
+processor = WhisperProcessor.from_pretrained("openai/whisper-tiny.en")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny.en").to(device)
+
+input_features = processor(
+    waveform, sampling_rate=sampling_rate, return_tensors="pt"
+).input_features
+
+predicted_ids = model.generate(input_features.to(device))
+
+transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+print(transcription)

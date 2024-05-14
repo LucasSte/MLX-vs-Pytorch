1eaebff1bc2acc641c9378347e5df0bf0ff466a3
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-05-14 11:50:27 -0300

    Finish whisper model

---
+++ mlx_models/whisper.py
@@ -0,0 +1,15 @@
+import mlx_whisper
+from datasets import load_dataset
+import mlx.core as mx
+
+ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+
+mx.set_default_device(mx.gpu)
+# TODO: Be sure to use the fp-32 model
+for i in range(0, 50):
+    audio_sample = ds[i]["audio"]
+    waveform = audio_sample["array"]
+    sampling_rate = audio_sample["sampling_rate"]
+
+    text = mlx_whisper.transcribe(waveform, path_or_hf_repo='./tiny_fp32', fp16=False)["text"]
+    print(text)
--- pytorch_models/whisper.py
+++ pytorch_models/whisper.py
@@ -2,21 +2,24 @@ from datasets import load_dataset
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 import torch
 
-device = torch.device('mps')
+device = torch.device('cpu')
 ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-audio_sample = ds[0]["audio"]
-waveform = audio_sample["array"]
-sampling_rate = audio_sample["sampling_rate"]
 
-processor = WhisperProcessor.from_pretrained("openai/whisper-tiny.en")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny.en").to(device)
 
-input_features = processor(
-    waveform, sampling_rate=sampling_rate, return_tensors="pt"
-).input_features
+for i in range(0, 50):
+    audio_sample = ds[i]["audio"]
+    waveform = audio_sample["array"]
+    sampling_rate = audio_sample["sampling_rate"]
 
-predicted_ids = model.generate(input_features.to(device))
+    processor = WhisperProcessor.from_pretrained("openai/whisper-tiny.en")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny.en").to(device)
 
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+    input_features = processor(
+        waveform, sampling_rate=sampling_rate, return_tensors="pt"
+    ).input_features
 
-print(transcription)
+    predicted_ids = model.generate(input_features.to(device))
+
+    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+    print(transcription)
